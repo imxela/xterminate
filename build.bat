a24330e1ab7951926dc5814e477651ff2e9db974
Alex Karlsson Lindén <65206220+alexkarlin@users.noreply.github.com>
2022-06-21 17:21:24 +0200

    Copy license file to output directory after build

--- build.bat
+++ build.bat
@@ -3,5 +3,6 @@
 CALL cargo build --release
 
 ROBOCOPY "%UserProfile%\Desktop\xterminate-master\res" "%UserProfile%\Desktop\xterminate-master\target\release\res" /E
+ROBOCOPY "%UserProfile%\Desktop\xterminate-master" "%UserProfile%\Desktop\xterminate-master\target\release" "LICENSE"
 
 PAUSE
