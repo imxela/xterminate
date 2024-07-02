9320e24b5fd0db022a5d8ee233efbc93566acdb5
imxela <65206220+imxela@users.noreply.github.com>
2024-07-02 19:34:44 +0200

    fix: disallow multiple instances
    Ensures only a single instance of xterminate is running at a time by displaying an error message if the user starts a new instance while one already exists.

--- src/main.rs
+++ src/main.rs
@@ -27,5 +27,60 @@ fn main() {
 
     error::set_panic_hook();
 
+    if instance_count() > 1 {
+        // An instance is already running
+        ui::taskdialog::TaskDialog::new()
+            .set_title("Already running")
+            .set_heading("Already running")
+            .set_content("An instance of xterminate is already running. Please exit the running instance before starting a new one.")
+            .set_icon(ui::taskdialog::TaskDialogIcon::InformationIcon)
+            .display()
+            .result();
+
+        return;
+    }
+
     App::run(&App::new());
+}
+
+/// Returns the amount of processes with the same file name as the current one.
+fn instance_count() -> u32 {
+    use windows::Win32::System::Diagnostics::ToolHelp::{
+        CreateToolhelp32Snapshot, Process32First, Process32Next, PROCESSENTRY32, TH32CS_SNAPPROCESS,
+    };
+
+    let mut count = 0;
+
+    unsafe {
+        let mut process_entry = PROCESSENTRY32 {
+            dwSize: std::mem::size_of::<PROCESSENTRY32>().try_into().unwrap(),
+            ..Default::default()
+        };
+
+        let snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0).unwrap();
+
+        let mut result = Process32First(snapshot, &mut process_entry);
+
+        loop {
+            if result.is_ok() {
+                if process_entry.szExeFile.starts_with(
+                    std::env::current_exe()
+                        .unwrap()
+                        .file_name()
+                        .unwrap()
+                        .as_encoded_bytes(),
+                )
+                // The rest should be nuls
+                {
+                    count += 1;
+                }
+            } else {
+                break;
+            }
+
+            result = Process32Next(snapshot, &mut process_entry);
+        }
+    }
+
+    count
 }
